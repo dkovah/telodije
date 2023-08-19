c6b0a09cdb2648164b036e84acbce2b97b24c111
dkovah <yoanpax@gmail.com>
2023-08-18 23:30:49 -0400

    Add localization provider from antd

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,16 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ConfigProvider } from "antd";
+import esEs from "antd/locale/es_ES";
 
 const queryClient = new QueryClient({});
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <QueryClientProvider client={queryClient}>
-      <Component {...pageProps} />
-    </QueryClientProvider>
+    <ConfigProvider locale={esEs}>
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
+      </QueryClientProvider>
+    </ConfigProvider>
   );
 }
